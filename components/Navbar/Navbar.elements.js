a5e276b9b8c2ddffa998ece5408af3938067c0dd
Gustavo Mendonça <contato.gustavomendonca@gmail.com>
2022-09-19 17:33:05 -0300

    Responsive Navbar

--- components/Navbar/Navbar.elements.js
+++ components/Navbar/Navbar.elements.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { DiMagento } from "react-icons/di";
 
 export const Nav = styled.nav`
   align-items: center;
@@ -7,10 +8,62 @@ export const Nav = styled.nav`
   display: flex;
   flex-direction: row;
   justify-content: space-around;
+  @media screen and (max-width: 960px) {
+    justify-content: space-between;
+  }
 `;
 
-export const Logo = styled.h1`
-  color: red;
+export const NavLogo = styled.div`
+  color: #fff;
+  justify-self: flex-start;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 2rem;
+  display: flex;
+  align-items: center;
+  margin-left: 10px;
+`;
+
+export const NavIcon = styled(DiMagento)`
+  margin-right: 0.5rem;
+`;
+
+export const MobileIcon = styled.div`
+  display: none;
+  @media screen and (max-width: 960px) {
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(-100%, 60%);
+    font-size: 1.8rem;
+    cursor: pointer;
+  }
+`;
+
+export const NavMenu = styled.ul`
+  display: flex;
+  align-items: center;
+  list-style: none;
+  text-align: center;
+  @media screen and (max-width: 960px) {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 90vh;
+    position: absolute;
+    top: 80px;
+    left: ${({ click }) => (click ? 0 : "-100%")};
+    opacity: 1;
+    transition: all 0.5s ease;
+    background: rgb(2, 0, 36);
+    background: linear-gradient(
+      90deg,
+      rgba(2, 0, 36, 1) 0%,
+      rgba(71, 70, 70, 1) 100%,
+      rgba(0, 212, 255, 1) 100%
+    );
+  }
 `;
 
 export const NavItem = styled.span`
@@ -23,8 +76,10 @@ export const NavItem = styled.span`
   &:hover {
     color: #57bdd1;
   }
-`;
 
-export const ItemName = styled.a`
-  text-decoration: none;
+  @media screen and (max-width: 960px) {
+    margin-left: 0;
+    font-size: 22px;
+    text-align: center;
+  }
 `;
--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,41 +1,79 @@
 import Link from "next/link";
-import { Nav, NavItem } from "./Navbar.elements";
+import {
+  Nav,
+  NavItem,
+  MobileIcon,
+  NavMenu,
+  NavIcon,
+  NavLogo,
+} from "./Navbar.elements";
+import { useState, useEffect } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import { IconContext } from "react-icons/lib";
 
 const Navbar = () => {
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  useEffect(() => {
+    // Client-side-only code
+    window.addEventListener("resize", showButton);
+  });
+
   return (
     <>
-      <Nav>
-        <div>
-          <Link href="/">
-            <NavItem>Logo</NavItem>
-          </Link>
-        </div>
-        <div>
-          <Link href="/">
-            <a>
-              <NavItem>Home</NavItem>
-            </a>
-          </Link>
+      <IconContext.Provider value={{ color: "#fff" }}>
+        <Nav>
+          <NavLogo>
+            <Link href="/" onClick={closeMobileMenu}>
+              <NavIcon />
+            </Link>
+            GMendonça
+          </NavLogo>
+          <MobileIcon onClick={handleClick}>
+            {click ? <FaTimes /> : <FaBars />}
+          </MobileIcon>
+          <NavMenu onClick={handleClick} click={click}>
+            <Link href="/">
+              <a>
+                <NavItem>Home</NavItem>
+              </a>
+            </Link>
 
-          <Link href="/experience">
-            <a>
-              <NavItem>Experience</NavItem>
-            </a>
-          </Link>
+            <Link href="/experience">
+              <a>
+                <NavItem>Experience</NavItem>
+              </a>
+            </Link>
 
-          <Link href="/contact">
-            <a>
-              <NavItem>Contact</NavItem>
-            </a>
-          </Link>
+            <Link href="/contact">
+              <a>
+                <NavItem>Contact</NavItem>
+              </a>
+            </Link>
 
-          <Link href="/resume">
-            <a>
-              <NavItem>Resume</NavItem>
-            </a>
-          </Link>
-        </div>
-      </Nav>
+            <Link href="/resume">
+              <a>
+                <NavItem>Resume</NavItem>
+              </a>
+            </Link>
+          </NavMenu>
+        </Nav>
+      </IconContext.Provider>
     </>
   );
 };
